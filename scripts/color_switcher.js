9b3ae7e397394c68d54f6135eba865f61440fda3
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2022-10-04 14:09:03 +0530

    First Upload

---
+++ scripts/color_switcher.js
@@ -0,0 +1,74 @@
+/*--- Styles Switcher ---*/
+
+// get JSON url
+var WpJsonUrl = document.querySelector('link[rel="https://api.w.org/"]').href
+// then take out the '/wp-json/' part
+var homeurl = WpJsonUrl.replace('/wp-json/','');
+
+
+jQuery(document).ready(function($) {
+	
+	$("#color_switcher_preview .stylesheet" ).click(function(){
+		$("#colors" ).attr("href", `${homeurl}/wp-content/themes/portfolio-listing/css/style.css` );
+		return false;
+	});
+
+	$("#color_switcher_preview .stylesheet_1" ).click(function(){
+		$("#colors" ).attr("href", `${homeurl}/wp-content/themes/portfolio-listing/css/colors_version/stylesheet_1.css` );
+		return false;
+	});
+	
+	$("#color_switcher_preview .stylesheet_2" ).click(function(){
+		$("#colors" ).attr("href", `${homeurl}/wp-content/themes/portfolio-listing/css/colors_version/stylesheet_2.css` );
+		return false;
+	});
+	
+	$("#color_switcher_preview .stylesheet_3" ).click(function(){
+		$("#colors" ).attr("href", `${homeurl}/wp-content/themes/portfolio-listing/css/colors_version/stylesheet_3.css` );
+		return false;
+	});
+	
+	$("#color_switcher_preview .stylesheet_4" ).click(function(){
+		$("#colors" ).attr("href", `${homeurl}/wp-content/themes/portfolio-listing/css/colors_version/stylesheet_4.css` );
+		return false;
+	});
+	
+	$("#color_switcher_preview .stylesheet_5" ).click(function(){
+		$("#colors" ).attr("href", `${homeurl}/wp-content/themes/portfolio-listing/css/colors_version/stylesheet_5.css` );
+		return false;
+	});
+	
+	$("#color_switcher_preview .stylesheet_6" ).click(function(){
+		$("#colors" ).attr("href", `${homeurl}/wp-content/themes/portfolio-listing/css/colors_version/stylesheet_6.css` );
+		return false;
+	});	
+	
+	$("#color_switcher_preview h2 a").click(function(e){
+		e.preventDefault();
+		var div = $("#color_switcher_preview");
+		// console.log(div.css("right"));
+		if (div.css("right") === "-210px") {
+			$("#color_switcher_preview").animate({
+				right: "0px"
+			}); 
+		} else {
+			$("#color_switcher_preview").animate({
+				right: "-210px"
+			});
+		}
+	});
+
+	$(".colors li a").click(function(e){
+		e.preventDefault();
+		$(this).parent().parent().find("a").removeClass("active");
+		$(this).addClass("active");
+	});
+	
+	$("#reset a").click(function(e){
+		e.preventDefault();
+		$(".colors li a" ).removeClass("active");
+		$("#colors" ).attr("href", "css/colors/style.css" );
+		$(window).resize();
+	});
+	
+});
